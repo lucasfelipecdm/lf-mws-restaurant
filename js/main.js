668cabc2180d9e7e41008bafa713103ebdeb6574
Lucas Felipe Costa da Mata <lucasfelipecdm@gmail.com>
2019-02-17 11:01:10 -0300

    feat: First Udacity Review - Fix

--- js/main.js
+++ js/main.js
@@ -179,7 +179,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = `Image of ${restaurant.name} restaurant`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -217,16 +217,11 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 
 } 
 
-setTabindexToMap = (markersMap, zoomMap, mapAll) => {
-  markersMap = document.querySelectorAll('.leaflet-marker-icon');
+setTabindexToMap = (zoomMap, mapAll) => {
   zoomMap = document.querySelectorAll('.leaflet-control-zoom a');
   mapAll = document.querySelector('#map');
 
   mapAll.setAttribute('tabindex','-1');
-  
-  markersMap.forEach(markerOnMap => {
-    markerOnMap.setAttribute('tabindex','-1');
-  });
 
   zoomMap.forEach(zoomBtn => {
     zoomBtn.setAttribute('tabindex','-1');
--- sw_cache_site.js
+++ sw_cache_site.js
@@ -1,15 +1,29 @@
-const nameCache = 'cache-v1'
+const nameCache = 'cache-v2'
+const cacheArray = [
+    'index.html',
+    'restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json'
+]
 /**
  * Call install event
  */
-self.addEventListener('install', () => {})
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(nameCache).then((cache) => {
+            cache.addAll(cacheArray)
+        }).then(() => self.skipWaiting())
+    )
+})
 
 /**
- * Call activate event
+ * Call activate event to remove the old cache
  */
 
 self.addEventListener('activate', (event) => {
-    //remove unwanted caches
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
@@ -22,22 +36,12 @@ self.addEventListener('activate', (event) => {
         })
     );
 })
+/**
+ * Call fetch event to respond the site request with the cache assest
+ */
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        fetch(event.request)
-            .then(res => {
-                //make a copy/clone of response
-                const resClone = res.clone();
-                //open the cache
-                caches
-                    .open(nameCache)
-                    .then(cache => {
-                        //add a response to cache
-                        cache.put(event.request, resClone);
-                    })
-                return res;
-            })
-            .catch(err => caches.match(event.request).then(res => res))
+        fetch(event.request).catch(() => caches.match(event.request))
         );
 })
